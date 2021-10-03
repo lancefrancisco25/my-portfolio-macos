7c5b6276cdf64004e9a215f8e806e3e2fde14274
lancefrancisco25 <lancefrancisco2507@gmail.com>
2021-10-03 21:59:04 +0800

    remove the div after video plays and fix video

--- components/preloader/preloader.js
+++ components/preloader/preloader.js
@@ -4,6 +4,7 @@ import css from './preloader.module.css'
 function preloader() {
     var clickStart = useRef(null);
     var video = useRef(null);
+    var mainCont = useRef(null);
     function StartApp(){
         clickStart.current.style.transform = 'scale(0.6)';
         clickStart.current.style.opacity = '0';
@@ -14,17 +15,17 @@ function preloader() {
     }
     useEffect(()=>{
         video.current.onended = function() {
-            video.current.style.display = 'none';
+            mainCont.current.style.display = 'none';
         };
     })
 
     return (
-        <div>
+        <div ref={mainCont}>
             <div className={css.startPre} ref={clickStart} onClick={StartApp}>
                 <h1>Press anywhere to start</h1>
             </div>
             <div className={css.videoCont}>
-                <video ref={video} style={{width: '100%', height: '100%', objectFit: 'cover'}} src="https://res.cloudinary.com/duw2s4w0s/video/upload/v1633263036/macos_wallpaper_2_enuuqh.mp4"/> 
+                <video ref={video} style={{width: '100%', height: '100%', objectFit: 'cover'}} src="https://res.cloudinary.com/duw2s4w0s/video/upload/v1633263036/macos_wallpaper_2_enuuqh.mp4" playsinline/> 
             </div>
         </div>
     )
